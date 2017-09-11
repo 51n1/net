09110b13855fac33206ae8c762b2de067ddc4519
51n1 <31303070+51n1@users.noreply.github.com>
2017-09-11 15:26:17 +0900

    Update drawingmotion3d.js

--- snapshots/drawingmotion3d.js
+++ snapshots/drawingmotion3d.js
@@ -26,7 +26,7 @@ function setup() {
   var fov = 60 / 180 * PI;
   var cameraZ = (height/2.0) / tan(fov/2.0);
   //perspective(60 / 180 * PI, width/height, cameraZ * 0.1, cameraZ * 10);
-  perspective(30 / 180 * PI, width/height, cameraZ * 0.1, cameraZ * 10);
+  perspective(30 / 180 * PI, width/height, 1, 10000);
 }
 
 function draw() {
