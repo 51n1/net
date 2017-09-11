222b38c5550b896108c088129774ab6b6de4202e
51n1 <31303070+51n1@users.noreply.github.com>
2017-09-11 17:14:58 +0900

    Update drawingmotion3d.js

--- snapshots/drawingmotion3d.js
+++ snapshots/drawingmotion3d.js
@@ -30,7 +30,7 @@ function draw() {
   if((on == false) && (pointer > 0)) { // After Memory
     //translate(0, 0, -pointer*zdepth*2)
     //camera(0, 0, pointer*zdepth);
-    orbitControl();
+    //orbitControl();
     rotateX(radians(map(mouseY, -height*0.5, height*0.5, 0, 360)));
     rotateY(radians(map(mouseX, -width*0.5, width*0.5, 0, 360)));
     background(10);
@@ -57,13 +57,13 @@ function draw() {
   if( pointer > 0 && (touches.length > 1 || keyIsDown(ENTER)) ) {
     pointer = 0;
     on = false;
+    select("#p5text").style("display", "block");
   }
 }
 
 function mousePressed() {
   if( pointer == 0 ) {
     on = true;
-    select("#p5text").style("display", "none");
   }
 }
 
@@ -84,5 +84,6 @@ function mouseReleased() {
     for (var i=0; i<pointer; i++){
       position[i][2] = pointer*zdepth*0.5 - i*zdepth;
     }
+    select("#p5text").style("display", "none");
   }
 }
