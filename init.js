fb8065dc5a72b6858ec05617cb4b12924e21df1d
51n1 <31303070+51n1@users.noreply.github.com>
2017-09-15 15:18:17 +0900

    Update init.js

--- init.js
+++ init.js
@@ -33,8 +33,10 @@ $("#controls>a:eq(4)").on('click', function(){ // Sketch List
 /* Initialize */
 function initP5sketch(p5name){
   var get = GetQueryString();
-  if (get["p5"]) p5name = get["p5"];
-  if (get["h"] != "undefined") helpFlag = get["h"];
+  if (get["p5"]) {
+    p5name = get["p5"];
+    helpFlag = get["h"];
+  }
   p5path = "./snapshots/" + p5name + ".js";
   loadP5sketch(p5path);
 
