c8fe72b3dda8e8ae52ab9f680c6474961b84a92d
51n1 <31303070+51n1@users.noreply.github.com>
2017-09-18 17:51:08 +0900

    Update drawingmotion3d.js

--- snapshots/drawingmotion3d.js
+++ snapshots/drawingmotion3d.js
@@ -3,7 +3,6 @@ var pointer; // int
 var amount; // float
 var zdepth; // float
 var on; // boolean
-var p5title = "Drawing + Motion + 3D + Audio";
 var hint1 = "Keep dragging on screen, and then if release, drawn lines will start moving with sound. Sound on with button of menu bar.";
 var hint2 = "You can turn a drawing object by sliding your mouse or finger. If make new one, push spacebar key or touch with two fingers.";
 var mysound, analyzer;
@@ -15,9 +14,10 @@ function preload() {
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.parent("p5canvas");
-  select("body").style("background-color", color(10));
-  select("#p5title").html(p5title);
+  var p5title = "Drawing + Motion + 3D + Audio";
   document.title = p5title + " - " + document.title;
+  select("#p5title").html(p5title);
+  select("body").style("background-color", color(10));
   select("#p5help").html(hint1);
 
   position = new Array(500);
