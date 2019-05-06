f18e7172c36dce6e72d8de3ca8cd5e7b511fa23b
51n1 <31303070+51n1@users.noreply.github.com>
2019-05-06 23:22:29 +0800

    add outer circle

--- snapshots/rotationofcircle.js
+++ snapshots/rotationofcircle.js
@@ -1,5 +1,11 @@
-let radius;
-let rotation;
+// Rotation of Circle
+// Created: 2017/09/05
+// Updated: 2019/05/06
+
+let innerRadius, outerRadius;
+let innerRotation = 0;
+let outerRotation = 0;
+let outerPositions;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
@@ -9,29 +15,50 @@ function setup() {
   select("#p5title").html(title);
   select("body").style("background-color", color(255));
 
-  radius = width > height ? height*0.4 : width*0.4;
-  rotation = 0;
+  innerRadius = width > height ? height * 0.25 : width * 0.25;
+  outerRadius = width > height ? height * 0.4 : width * 0.4;
+
+  outerPositions = new Array(36);
+  for (let i = 0; i < 360; i += 10) {
+    outerPositions[i] = new Array(2);
+  }
 }
 
 function draw() {
   background(255);
-  beginShape();
+  stroke(0);
+  noFill();
+
+  // Outer Circle
+  //beginShape();
   for(let i = 0; i < 360; i += 10) {
-    let angle = i - rotation;
-    let x = width*0.5 + radius * sin(radians(angle));
-    let y = height*0.5 + radius * cos(radians(angle));
-
-    stroke(0);
-    noFill();
-    vertex(x, y);
-
+    let angle = i - outerRotation;
+    outerPositions[i][0] = width * 0.5 + outerRadius * sin(radians(angle));
+    outerPositions[i][1] = height * 0.5 + outerRadius * cos(radians(angle));
+    let x = outerPositions[i][0];
+    let y = outerPositions[i][1];
+    //vertex(x, y);
     ellipse(x, y, 5, 5);
   }
-  endShape(CLOSE);
-  rotation += 0.5;
+  //endShape(CLOSE);
+  outerRotation += 0.2;
+
+  // Inner Circle
+  //beginShape();
+  for(let i = 0; i < 360; i += 10) {
+    let angle = i - innerRotation;
+    let x = width * 0.5 + innerRadius * sin(radians(angle));
+    let y = height * 0.5 + innerRadius * cos(radians(angle));
+    //vertex(x, y);
+    ellipse(x, y, 5, 5);
+    line(x, y, outerPositions[i][0], outerPositions[i][1])
+  }
+  //endShape(CLOSE);
+  innerRotation += 0.6;
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  radius = width > height ? height*0.4 : width*0.4;
+  innerRadius = width > height ? height * 0.25 : width * 0.25;
+  outerRadius = width > height ? height * 0.4 : width * 0.4;
 }
